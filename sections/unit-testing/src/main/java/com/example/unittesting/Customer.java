8280f0712c5003b775674959424687cb728907c4
Josh Long <josh@joshlong.com>
2019-01-26 16:48:08 -0800

    adding the unit testing example

---
+++ sections/unit-testing/src/main/java/com/example/unittesting/Customer.java
@@ -0,0 +1,14 @@
+package com.example.unittesting;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+	private String id;
+	private String name;
+}
---
+++ sections/unit-testing/src/test/java/com/example/unittesting/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.example.unittesting;
+
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class CustomerTest {
+
+	@Test
+	public void createWithConstructorWithParams() throws Exception {
+
+		Customer customer = new Customer("id", "name");
+
+		Assert.assertEquals(customer.getId(), "id");
+		Assert.assertNotNull(customer.getId());
+
+		Assert.assertThat(customer.getName(), Matchers.not(Matchers.isEmptyString()));
+		Assert.assertThat(customer.getId(), Matchers.is("id"));
+
+		Assertions.assertThat(customer.getId()).isNotBlank();
+		Assertions.assertThat(customer.getId()).isEqualTo("id");
+
+	}
+}
