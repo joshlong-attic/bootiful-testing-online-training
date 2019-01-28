3cd96b8c6a86963e73b2a8edbea9202bbb25f407
Josh Long <josh@joshlong.com>
2019-01-28 02:23:52 -0800

    polish.

--- sections/web/mvc/src/test/java/mvc/GreetingsRestControllerTest.java
+++ sections/web/mvc/src/test/java/mvc/GreetingsRestControllerTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,9 +26,9 @@ public class GreetingsRestControllerTest {
 	public void greet() throws Exception {
 
 		this.mockMvc
-			.perform(request(HttpMethod.GET, "/greetings"))
+			.perform(request(GET, "/greetings"))
 			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+			.andExpect(content().contentType(APPLICATION_JSON_UTF8_VALUE))
 			.andExpect(jsonPath(".message").value("Hello!"));
 
 	}
@@ -37,9 +37,9 @@ public class GreetingsRestControllerTest {
 	public void greetJane() throws Exception {
 
 		this.mockMvc
-			.perform(request(HttpMethod.GET, "/greetings/Jane"))
+			.perform(request(GET, "/greetings/Jane"))
 			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+			.andExpect(content().contentType(APPLICATION_JSON_UTF8_VALUE))
 			.andExpect(jsonPath(".message").value("Hello Jane!"));
 	}
 }
