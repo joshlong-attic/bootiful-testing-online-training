1a3246e95f1f854d24136a3392b7d41ef0fcd698
Josh Long <josh@joshlong.com>
2019-02-03 14:25:21 -0800

    removing the supporting scaffolding.

--- spring-security-testing/method/src/main/java/livelessons/MessageService.java
+++
@@ -1,7 +0,0 @@
-package livelessons;
-
-public interface MessageService {
-
-	String getMessage();
-
-}
--- testing-101-from-acdll/customer-service/src/main/java/com/example/customerservice/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customerservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
-	* @author <a href="josh@joshlong.com">Josh Long</a>
-	*/
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
